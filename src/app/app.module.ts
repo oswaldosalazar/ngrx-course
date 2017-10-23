59e2d9329287a76d639111a9b24227375146b56b
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-10-22 18:46:16 -0600

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserSelectionComponent,
+    ThreadSectionComponent,
+    MessageSectionComponent,
+    MessageListComponent,
+    ThreadListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
